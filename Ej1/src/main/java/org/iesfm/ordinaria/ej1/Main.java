72e168b7546b22a0e079bffd17762c77b980c442
mikel <mikelsanvi@gmail.com>
2021-05-29 10:47:19 +0200

    first commit

---
+++ Ej1/src/main/java/org/iesfm/ordinaria/ej1/Main.java
@@ -0,0 +1,11 @@
+package org.iesfm.ordinaria.ej1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    // Ejemplo de creación de log4j
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+
+}
